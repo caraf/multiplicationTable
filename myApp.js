97fe72ea08aaaa0d834ec6a55e4d90d8dd1f028b
Cara Franco <xbbkbnv@eagleinvsys.com>
2015-11-30 21:26:27 -0500

    Dynamic size.

--- myApp.js
+++ myApp.js
@@ -1,7 +1,21 @@
 angular.module('myApp', [])
-    .controller('MultiplicationCtrl', function($scope) {
+    .controller('MultiplicationCtrl', function($scope, $attrs) {
 
-    $scope.numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+    $scope.numbers = function() {
+        var numbers = [];
+	for (var i = 0; i < $scope.numberLimit; i++)
+	{
+	    numbers[i] = i + 1;
+	}
+	return numbers;
+    };
+
+    $scope.compute = function(a, b) {
+        return a * b;
+    }
+
 
 });
 
